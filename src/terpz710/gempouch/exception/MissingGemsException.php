785db46413338cef378edda0364dbed771bf8c01
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2025-01-04 15:33:44 -0800

    Update MissingGemsException.php

--- src/terpz710/gempouch/exception/MissingGemsException.php
+++ src/terpz710/gempouch/exception/MissingGemsException.php
@@ -9,7 +9,7 @@ use RuntimeException;
 final class MissingGemsException extends RuntimeException {
 
     public function __construct(string $gems, int $code = 0, \Throwable $previous = null) {
-        $message = "The required plugin '{$gems}' is missing, Download the plugin via Poggit or GitHub!";
+        $message = "The required plugin '" . $gems . "' is missing, Download the plugin via Poggit or GitHub!";
         parent::__construct($message, $code, $previous);
     }
 }
